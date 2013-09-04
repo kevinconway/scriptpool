f468f6defcd87fb5cb438870f5cccfae0321fa04
Kevin Conway <kevinjacobconway@gmail.com>
2013-09-03 21:10:20 -0500

    Add: Logging calls and use of new Worker resource
    Signed-off-by: Kevin Conway <kevinjacobconway@gmail.com>

--- scriptpool/pool.sh
+++ scriptpool/pool.sh
@@ -4,16 +4,19 @@
 # worker pool for executing tasks.
 
 # Load utilities module.
-source utilities.sh
+source ./utilities.sh
+
+# Load the logging module.
+source ./logging.sh
 
 # Load worker library.
-source workers.sh
+source ./worker.sh
 
 Pool () {
 
   # Setup for argument parsing.
   local short="h"
-  local long="help,workers:,taskfile:,queuedir:,pipefile:"
+  local long="help,workers:,taskfile:,queuedir:,pipefile:,logfile:,terminator:,poll:"
   local args=$(getopt -o "$short" --long "$long"  -- "$@")
   if [[ $? != 0 ]];
   then
@@ -29,9 +32,11 @@ Pool () {
   local taskfile=""
   local queuedir="~/.poolq"
   local pipefile="~/.pool"
+  local logfile="~/pool.log"
+  local terminator="terminate_pool"
+  local poll=".1"
   local cmd=""
   local counter="0"
-  local poll=".1"
 
   while true;
   do
@@ -52,6 +57,9 @@ arguments:
   --taskfile:       Library file for workers to source.
   --queuedir:       Directory to place worker queues (default ~/.poolq).
   --pipefile:       Named pipe used to issue tasks (default ~/.pool).
+  --logfile:        File to keep log data (default ~/pool.log).
+  --terminator:     Stop command (default 'terminate_pool').
+  --poll:           Idle wait time for worker when queue is empty (default .1).
 BLOCK
 
         echo "$help_message"
@@ -74,6 +82,18 @@ BLOCK
         pipefile="$2"
         shift 2;;
 
+      --logfile)
+        logfile="$2"
+        shift 2;;
+
+      --terminator)
+        terminator="$2"
+        shift 2;;
+
+      --poll)
+        poll="$2"
+        shift 2;;
+
       --)
         shift
         break;;
@@ -83,11 +103,15 @@ BLOCK
   taskfile="$(_absolute_path "$taskfile")"
   queuedir="$(_absolute_path "$queuedir")"
   pipefile="$(_absolute_path "$pipefile")"
+  logfile="$(_absolute_path "$logfile")"
+
+  DEBUG "$logfile" "Pool called with arguments: workers=$workers taskfile=$taskfile queuedir=$queuedir pipefile=$pipefile logfile=$logfile terminator=$terminator poll=$poll."
+  INFO "$logfile" "Attempting to start new pool with $workers workers."
 
   # Check that taskfile exists.
   if [[ ! -e "$taskfile" ]]; then
 
-    echo "Taskfile ($taskfile) not found."
+    ERROR "$logfile" "Taskfile ($taskfile) not found."
     exit 1
 
   fi
@@ -95,7 +119,7 @@ BLOCK
   # Make sure pipe isn't a regular file.
   if [[ -e "$pipefile" && ! -p "$pipefile" ]]; then
 
-    echo "The pipe file ($pipefile) already exists and is not a FIFO."
+    ERROR "$logfile" "The pipe file ($pipefile) already exists and is not a FIFO."
     exit 1
 
   fi
@@ -103,11 +127,11 @@ BLOCK
   # Create pipe if not already created.
   if [[ ! -e "$pipefile" ]]; then
 
-    mkfifo "$pipefile"
+    mkfifo "$pipefile" 1>&2 2>/dev/null
 
     if [[ $? != 0 ]];
     then
-      echo "Could not create named pipe at ($pipefile)."
+      ERROR "$logfile" "Could not create named pipe at ($pipefile)."
       exit 1
     fi
 
@@ -120,45 +144,136 @@ BLOCK
 
     if [[ $? != 0 ]];
     then
-      echo "Could not create queue dir ($queuedir)."
+      ERROR "$logfile" "Could not create queue dir ($queuedir)."
       exit 1
     fi
 
   fi
 
-  # Spin off workers.
-  _start_workers \
-    --workers="$workers" \
-    --taskfile="$taskfile" \
-    --queuedir="$queuedir" \
-    --poll="$poll"
+  for (( i = 0; i < "$workers"; i+=1 )); do
 
-  if [[ $? != 0 ]];
-  then
-    echo "Failed to create workers."
-    exit 1
-  fi
+      INFO "$logfile" "Starting worker ($i)."
+      Worker \
+        --taskfile="$taskfile" \
+        --queuefile="$queuedir/$i.tasks" \
+        --logfile="$logfile" \
+        --terminator="$terminator" \
+        --poll="$poll" \
+        &
+
+  done
 
   # Add traps for signals to handle cleanup of workers.
-  trap "_stop_workers --workers=$workers --queuedir=$queuedir" SIGINT
+  trap "_stop_workers --workers=$workers --queuedir=$queuedir --logfile=$logfile --terminator=$terminator" SIGINT
 
   # Listen for messages and dispatch them.
+  INFO "$logfile" "Pool entering action loop."
   while read cmd < "$pipefile"
   do
 
-    if [[ "$cmd" == "terminate_pool" ]]; then
+    DEBUG "$logfile" "Pool found new message ($cmd)."
+
+    if [[ "$cmd" == "$terminator" ]]; then
+
+      INFO "$logfile" "Pool received terminator ($terminator). Shutting down."
 
       _stop_workers \
         --workers="$workers" \
-        --queuedir="$queuedir"
+        --queuedir="$queuedir" \
+        --logfile="$logfile" \
+        --terminator="$terminator"
 
       break
 
     fi
 
+    DEBUG "$logfile" "Dispatching ($cmd) to worker ($counter)."
     echo "$cmd" >> "$queuedir/$counter.tasks"
     counter=$(($counter + 1))
     counter=$(($counter % $workers))
+  done
+
+}
+
+_stop_workers () {
+
+  # Setup for argument parsing.
+  local short="h"
+  local long="help,workers:,queuedir:,logfile:,terminator:"
+  local args=$(getopt -o "$short" --long "$long"  -- "$@")
+  if [[ $? != 0 ]];
+  then
+    echo "Failure in getopt."
+    exit 1
+  fi
+
+  # Set vars in scope.
+  eval set -- "$args"
+
+  # Set local vars.
+  local workers=1
+  local queuedir="~/.poolq"
+  local logfile="~/pool.log"
+  local terminator="terminate_pool"
+
+  while true;
+  do
+    case "$1" in
+
+      -h|--help)
+
+        local help_message=""
+
+        read -d '' help_message <<"BLOCK"
+Stop a pool of workers.
+
+Note: This function should not be called directly.
+
+_stop_workers [arguments]
+
+arguments:
+
+  --workers:        Number of workers to stop (default 1).
+  --queuedir:       Directory where worker queues are placed (default ~/.poolq).
+  --logfile:        File to keep log data (default ~/pool.log).
+  --terminator:     Stop command (default 'terminate_pool').
+BLOCK
+
+        echo "$help_message"
+        exit 1
+        shift;;
+
+      --workers)
+        workers="$2"
+        shift 2;;
+
+      --queuedir)
+        queuedir="$2"
+        shift 2;;
+
+      --logfile)
+        logfile="$2"
+        shift 2;;
+
+      --terminator)
+        terminator="$2"
+        shift 2;;
+
+      --)
+        shift
+        break;;
+    esac
+  done
+
+  taskfile="$(_absolute_path "$taskfile")"
+  queuedir="$(_absolute_path "$queuedir")"
+  logfile="$(_absolute_path "$logfile")"
+
+  for (( i = 0; i < "$workers"; i++ )); do
+
+    INFO "$logfile" "Pool sending terminator to worker ($i)."
+    echo "$terminator" > "$queuedir/$i.tasks"
+
   done
 
 }
